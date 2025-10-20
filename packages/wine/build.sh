7fdd1d35d0124147d72ded8e2e622f5a7e56b205
Honkonx <116902904+Honkonx@users.noreply.github.com>
2025-10-20 04:18:38 +0200

    Update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,11 +1,11 @@
-PKG_VER="10.0-esync-xinput-dinput"
+PKG_VER="9.0-esync-xinput-dinput"
 PKG_CATEGORY="Wine"
-PKG_PRETTY_NAME="Wine-proton ($PKG_VER)"
+PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/proton-wine
-GIT_COMMIT=master
+GIT_URL=https://github.com/KreitinnSoftware/wine
+GIT_COMMIT=wine-9.0
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
